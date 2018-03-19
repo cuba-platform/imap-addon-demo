6a712818a0405574700ad80f2b3a8cf5478a5961
maksim.shmelev <shmelev@haulmont.com>
2018-03-19 13:22:22 +0400

    IMAP-55 API для email attachments use postgresql

---
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -0,0 +1,27 @@
+-- begin IMAPSAMPLE_IMAP_MESSAGE
+create table IMAPSAMPLE_IMAP_MESSAGE (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    SEEN boolean,
+    FROM_ varchar(255),
+    TO_LIST varchar(255),
+    CC_LIST varchar(255),
+    BCC_LIST varchar(255),
+    SUBJECT varchar(255),
+    FLAGS_LIST varchar(255),
+    DATE_ timestamp,
+    SEEN_TIME timestamp,
+    MESSAGE_UID bigint not null,
+    FOLDER_NAME varchar(255) not null,
+    MAIL_BOX_ID uuid,
+    --
+    primary key (ID)
+)^
+-- end IMAPSAMPLE_IMAP_MESSAGE
